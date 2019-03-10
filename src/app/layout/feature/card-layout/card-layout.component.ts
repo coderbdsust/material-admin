1397d0a8009a47d47783b874a46623c78fb65104
Biswajit Debnath <biswajit@kinetik.care>
2019-03-10 23:10:07 +0600

    card layout init

---
+++ src/app/layout/feature/card-layout/card-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-layout',
+  templateUrl: './card-layout.component.html',
+  styleUrls: ['./card-layout.component.scss', '../../layout.component.scss']
+})
+export class CardLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
