9c8ea2ea1e1d259673682958bfc602d9ed334e70
Biswajit Debnath <biswajit@kinetik.care>
2019-03-10 21:52:43 +0600

    Layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import {SideNavService} from './service/side-nav.service';
 import {MatListModule, MatList} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, ToolbarComponent],
+  declarations: [AppComponent, SidebarComponent, ToolbarComponent, LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { SideNavService } from '../service/side-nav.service';
-
+import { Component, OnInit } from "@angular/core";
+import { SideNavService } from "../service/side-nav.service";
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  selector: "app-toolbar",
+  templateUrl: "./toolbar.component.html",
+  styleUrls: ["./toolbar.component.scss"]
 })
 export class ToolbarComponent implements OnInit {
-  constructor(private sidenav: SideNavService) { }
+  constructor(private sidenav: SideNavService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   toggleRightSidenav() {
-    console.log('toggleRightSidenav');
     this.sidenav.toggle();
- }
-
-
+  }
 }
