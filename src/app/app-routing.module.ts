de18a50dbd277d08408e65c3b6ff3795166bbaa4
Biswajit Debnath <biswajit@kinetik.care>
2019-03-11 10:18:59 +0600

    service , mutiple route remeved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { FormLayoutComponent } from "./layout/feature/form-layout/form-layout.co
 import { ROUTE_NAME } from "./routes/Routes";
 
 const routes: Routes = [
-  { path: ROUTE_NAME.HOME, component: CardLayoutComponent },
   { path: ROUTE_NAME.SOCIAL, component: CardLayoutComponent },
   { path: ROUTE_NAME.TABLE, component: FormLayoutComponent }
 ];
