fb1123959a5626a659b94ce3fedc6517a9b25228
Biswajit Debnath <biswajit@kinetik.care>
2019-03-11 18:05:49 +0600

    Toolbar search bard added

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -4,7 +4,7 @@ import { SideNavService } from "../service/side-nav.service";
 @Component({
   selector: "app-toolbar",
   templateUrl: "./toolbar.component.html",
-  styleUrls: ["./toolbar.component.scss"]
+  styleUrls: ["./toolbar.component.scss", '../layout/layout.component.scss']
 })
 export class ToolbarComponent implements OnInit {
   constructor(private sidenav: SideNavService) {}
