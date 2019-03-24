c3a26033d079986451d602d6fda54519157a408c
Biswajit Debnath <biswajit@kinetik.care>
2019-03-24 21:43:49 +0600

    admin page componant added

--- src/app/layout/feature/admin-layout/admin-layout.component.ts
+++ src/app/layout/feature/admin-layout/admin-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js";
+import { ChartColor } from "../graph/color.const";
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,9 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
+  linechart1: any;
+  linechart2: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.linechart1 = new Chart("linechart-1", {
+      type: "line",
+      data: {
+        labels: ["2010", "2011", "2012", "2013", "2014", "2015", "2016"],
+        datasets: [
+          {
+            backgroundColor: ChartColor.RED,
+            borderColor: ChartColor.RED,
+            fill: false,
+            label: "Apple",
+            data: [4, 3, 4, 5, 6, 9, 3],
+            borderWidth: 3
+          },
+          {
+            backgroundColor: ChartColor.GREEN,
+            borderColor: ChartColor.GREEN,
+            fill: false,
+            label: "Google",
+            data: [8, 6, 4, 5, 6, 9, 1],
+            borderWidth: 3
+          },
+          {
+            backgroundColor: ChartColor.BLUE,
+            borderColor: ChartColor.BLUE,
+            fill: false,
+            label: "Facebook",
+            data: [5, 7, 4, 2, 6, 9, 10],
+            borderWidth: 3
+          }
+        ]
+      },
+      options: {}
+    });
+    this.linechart2 = new Chart("linechart-2", {
+      type: "bar",
+      data: {
+        labels: ["2010", "2011", "2012", "2013", "2014", "2015", "2016"],
+        datasets: [
+          {
+            backgroundColor: ChartColor.RED,
+            borderColor: ChartColor.RED,
+            fill: false,
+            label: "Apple",
+            data: [4, 3, 4, 5, 6, 9, 3],
+            borderWidth: 3
+          },
+          {
+            backgroundColor: ChartColor.GREEN,
+            borderColor: ChartColor.GREEN,
+            fill: false,
+            label: "Google",
+            data: [8, 6, 4, 5, 6, 9, 1],
+            borderWidth: 3
+          },
+          {
+            backgroundColor: ChartColor.BLUE,
+            borderColor: ChartColor.BLUE,
+            fill: false,
+            label: "Facebook",
+            data: [5, 7, 4, 2, 6, 9, 10],
+            borderWidth: 3
+          }
+        ]
+      },
+      options: {}
+    });
   }
 
 }
