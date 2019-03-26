6f96390e500c9648f7b4e71e8da01f949dd030a6
Biswajit Debnath <biswajit@kinetik.care>
2019-03-26 10:42:26 +0600

    graph color alpha increase

--- src/app/layout/feature/graph/color.const.ts
+++ src/app/layout/feature/graph/color.const.ts
@@ -1,9 +1,9 @@
 export const ChartColor = {
-  RED: "rgba(255,99,132,0.2)",
-  GRAY: "rgba(128,128,128, 0.2)",
-  GREEN: "rgba(0, 128,0, 0.2)",
-  YELLOW: "rgba(255,255,0, 0.2)",
-  BLACK: "rgba(0,0,0,0.2)",
-  ORANGE: "rgba(255,127,80,0.2)",
-  BLUE: "rgba(0,0,255,0.2)"
+  RED: "rgba(255,99,132,0.4)",
+  GRAY: "rgba(128,128,128, 0.4)",
+  GREEN: "rgba(0, 128,0, 0.4)",
+  YELLOW: "rgba(255,255,0, 0.4)",
+  BLACK: "rgba(0,0,0,0.4)",
+  ORANGE: "rgba(255,127,80,0.4)",
+  BLUE: "rgba(0,0,255,0.4)"
 };
