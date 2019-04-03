e11f1581487ea14b9843e63c9825e888c850342b
Biswajit Debnath <biswajit@kinetik.care>
2019-04-03 21:07:52 +0600

    navigation sidebar added for color picker

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit, NgModule} from '@angular/core';
 import { SideNavService } from '../service/side-nav.service';
+import { ColorPickerService } from 'src/app/service/color-picker.service';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  
+  public closeSetting: boolean = true;
 
-  constructor(private sidenavService: SideNavService) { }
+  constructor(private sidenavService: SideNavService,private colorPicker: ColorPickerService) { }
+
+  public onChooseColor(action){
+    console.log(action);
+    this.colorPicker.setColor(action);
+  }
+
+  public getColor(){
+    return this.colorPicker.getColor();
+  }
 
   ngOnInit() {
+  }
+
+  toggle(){
+    console.log('toggle menu');
+    this.closeSetting=!this.closeSetting;
   }
 
 }
