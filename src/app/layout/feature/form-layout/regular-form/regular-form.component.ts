370f42d8ea2785647b56e2be7f0d4f93331819a9
Biswajit Debnath <biswajit@kinetik.care>
2019-04-09 14:37:03 +0600

    regular-form element added

--- src/app/layout/feature/form-layout/regular-form/regular-form.component.ts
+++ src/app/layout/feature/form-layout/regular-form/regular-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Designation {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-regular-form',
   templateUrl: './regular-form.component.html',
@@ -7,6 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegularFormComponent implements OnInit {
 
+  designations: Designation[] = [
+    { value: "junior software engineer", viewValue: "Junior Software Engineer" },
+    { value: "software engineer", viewValue: "Software Engineer" },
+    { value: "senior software engineer", viewValue: "Senior Software Engineer" },
+    { value: "system analyst", viewValue: "System Analyst" },
+    { value: "project manager", viewValue: "Project Manager" }
+  ];
   constructor() { }
 
   ngOnInit() {
