76e022fa6abc8992ef591702dcfa228561f4a7f0
Biswajit Debnath <biswajit@kinetik.care>
2019-04-10 10:09:07 +0600

    current location shown in google maps

--- src/app/layout/pages/maps/maps.component.ts
+++ src/app/layout/pages/maps/maps.component.ts
@@ -9,9 +9,18 @@ export class MapsComponent implements OnInit {
   lat1: number = 51.678418;
   lng1: number = 7.809007;
   lat2: number = 51.678418;
-  lng3: number = 7.809007;
-  
-  constructor() { }
+  lng2: number = 7.809007;
+  curLat: any;
+  curLng: any;
+
+  constructor() {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.curLng = +pos.coords.longitude;
+        this.curLat = +pos.coords.latitude;
+      });
+    }
+  }
 
   ngOnInit() {
   }
