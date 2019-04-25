d96e3b67d3cf5d6b7ea66484cde2348f4b0086a2
Biswajit Debnath <biswajit@kinetik.care>
2019-04-25 10:46:40 +0600

    admin and card updated

--- src/app/layout/feature/table-layout/data-table/data-table.component.ts
+++ src/app/layout/feature/table-layout/data-table/data-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
-import {PeriodicElement, ELEMENT_DATA } from '../table-data';
-import {SelectionModel} from '@angular/cdk/collections';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { PeriodicElement, ELEMENT_DATA } from '../table-data';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 @Component({
@@ -12,7 +12,7 @@ import {SelectionModel} from '@angular/cdk/collections';
 export class DataTableComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol', 'action'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
@@ -31,12 +31,25 @@ export class DataTableComponent implements OnInit {
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  editItem(row) {
+    console.log('editItem');
+    console.log(row);
+  }
+
+  removeItem(row) {
+    console.log('removeItem');
+    console.log(row);
+    let index: number = this.dataSource.data.findIndex(d => d === row);
+    console.log('remove index' + index);
+    this.dataSource.data.splice(index, 1);
   }
 
 }
