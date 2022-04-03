02b197ce8e82dd91431ae42c147f5fc9f36012ad
coderbdsust <biswajit.sust@gmail.com>
2022-04-03 13:05:24 +0200

    Image URL on Title, Desc, Subtitle  updated on Social Page with readme

--- src/app/layout/feature/social-layout/social/social-card.component.ts
+++ src/app/layout/feature/social-layout/social/social-card.component.ts
@@ -9,9 +9,17 @@ export class SocialComponent implements OnInit {
   @Input("socialCardImage")
   public socialCardImage = "assets/bird/birds-291a.jpg";
 
-  constructor() { }
+  @Input("socialCardTitle")
+  public socialCardTitle = "Hyacinth";
 
-  ngOnInit() {
-  }
+  @Input("socialCardSubTitle")
+  public socialCardSubTitle = "I am bold";
+
+  @Input("socialCardSubDesc")
+  public socialCardSubDesc = "The hyacinth macaw, or hyacinthine macaw, is a parrot native to central and eastern South America. It is the largest macaw and the largest flying parrot species";
+
+  constructor() {}
+
+  ngOnInit() {}
 
 }
