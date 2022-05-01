d83e4601645f01578c7826bba6e98fd8a429e4c1
coderbdsust <biswajit.sust@gmail.com>
2022-05-01 13:33:15 +0200

    Background Image Changed With Gallery Image

--- src/app/layout/feature/gallery/gallery.component.ts
+++ src/app/layout/feature/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryImageSize } from 'ngx-gallery';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryComponent } from 'ngx-gallery';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-gallery',
@@ -10,8 +11,10 @@ export class GalleryComponent implements OnInit {
 
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+  @ViewChild(NgxGalleryComponent) ngxGallery;
+  public backgroundStyle;
 
-  constructor() { }
+  constructor(private sanitizer:DomSanitizer) { }
 
   ngOnInit(): void {
 
@@ -26,7 +29,7 @@ export class GalleryComponent implements OnInit {
       {
         breakpoint: 800,
         width: '100%',
-        height: '750px',
+        height: '650px',
         imagePercent: 100,
         thumbnailsPercent: 20,
         thumbnailsMargin: 20,
@@ -84,6 +87,13 @@ export class GalleryComponent implements OnInit {
         big: 'https://cdn.glitch.global/ffed6655-f0c8-43de-b7fc-94a19528fcaf/butterfly-49a.jpg?v=1648999634554'
       }
     ];
+    this.currentImage();
   }
 
+  public currentImage() : void {
+    let idx=this.ngxGallery.selectedIndex;
+    let backgroundImage = this.galleryImages[idx].big;
+    this.backgroundStyle = this.sanitizer.bypassSecurityTrustStyle(`linear-gradient(to right bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backgroundImage}) no-repeat 0 0 / cover`);
+  }
+  
 }
