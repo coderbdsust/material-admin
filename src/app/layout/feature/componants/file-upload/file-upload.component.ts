41db22ba3d4e3f88b5dd2451408398e05fd145d0
Biswajit Debnath <biswajit.sust@gmail.com>
2022-06-02 08:31:53 +0200

    angular version updated to 13 and fix error of fileupload

--- src/app/layout/feature/componants/file-upload/file-upload.component.ts
+++ src/app/layout/feature/componants/file-upload/file-upload.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  UploadEvent,
-  UploadFile,
-  FileSystemFileEntry,
-  FileSystemDirectoryEntry
-} from "ngx-file-drop";
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+
 
 @Component({
   selector: "app-file-upload",
@@ -18,24 +14,30 @@ export class FileUploadComponent implements OnInit {
 
   handleChange(): void {}
 
-  public files: UploadFile[] = [];
+  public files: NgxFileDropEntry[] = [];
   
-  public dropped(event: UploadEvent) {
-    this.files = event.files;
-    for (const droppedFile of event.files) {
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
+
+      // Is it a file?
       if (droppedFile.fileEntry.isFile) {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         fileEntry.file((file: File) => {
+
           // Here you can access the real file
           console.log(droppedFile.relativePath, file);
+
           /**
           // You could upload it like this:
           const formData = new FormData()
           formData.append('logo', file, relativePath)
+
           // Headers
           const headers = new HttpHeaders({
             'security-token': 'mytoken'
           })
+
           this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
           .subscribe(data => {
             // Sanitized logo returned from backend
